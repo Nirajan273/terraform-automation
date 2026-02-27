4a2e728252489cd220dd972df0f7d519e3cf1859
Nirajan <nirajan.naryal@gmail.com>
2026-02-27 14:59:50 +1100

    removing all vms

--- live/team-alpha/main.tf
+++ live/team-alpha/main.tf
@@ -30,7 +30,7 @@ module "alpha_infra" {
   source = "../../modules/proxmox_vm"
 
   team_name      = "alpha"
-  vm_count       = 2
+  vm_count       = 0
   starting_vmid  = 400
   docker_image   = "nginx:latest"
   
