a77d62c2db012ed2ba028b8a248b36f1784dd63d
Nirajan <nirajan.naryal@gmail.com>
2026-02-27 11:02:42 +1100

    semi-automated

--- live/team-alpha/main.tf
+++ live/team-alpha/main.tf
@@ -30,7 +30,7 @@ module "alpha_infra" {
   source = "../../modules/proxmox_vm"
 
   team_name      = "alpha"
-  vm_count       = 2
+  vm_count       = 3
   starting_vmid  = 400
   docker_image   = "nginx:latest"
   
