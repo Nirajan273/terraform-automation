310ed9b19a4f275a6eb3dc752a567ae0f84788fe
Nirajan <nirajan.naryal@gmail.com>
2026-03-30 10:24:35 +1100

    creating a test infra for cassandra

---
+++ live/team-cassandra/main.tf
@@ -0,0 +1,44 @@
+# This tells Terraform who the provider is
+terraform {
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+      version = "3.0.2-rc07"
+      
+    }
+  }
+}
+
+variable "proxmox_api_url" { type = string }
+variable "proxmox_api_token_id" { type = string }
+variable "proxmox_api_token_secret" { 
+  type      = string 
+  sensitive = true 
+}
+
+# The Provider now uses these variables
+provider "proxmox" {
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+  pm_tls_insecure     = true
+  pm_parallel     = 3
+}
+
+# The Module Call
+module "nirajan_infra" {
+  source = "../../modules/proxmox_vm"
+
+  team_name      = "cassandra"
+  vm_count       = 0
+  starting_vmid  = 600
+  docker_image   = "cassandra:latest"
+  
+  # These refer to variables we'll set in the .tfvars file
+  admin_user     = var.proxmox_user 
+  admin_password = var.proxmox_password
+}
+
+# These variables pass the sensitive info from your env/tfvars to the module
+variable "proxmox_user" {}
+variable "proxmox_password" {}
