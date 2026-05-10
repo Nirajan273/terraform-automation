13c8a0fc78b9d04b5da98c0882baa3646d017517
nirajan273 <nirajanaryal273@gmail.com>
2026-05-10 11:09:49 +1000

    adding momonbitez frontend

--- live/team-cassandra/main.tf
+++ live/team-cassandra/main.tf
@@ -47,7 +47,13 @@ module "Cassandra_infra" {
       vmid  = 705,
       memory = 4192,
       disk_size= "16G"
+    },
+    "Momonbitez_hisab" = {
+      vmid      = 706, 
+      memory    = 8192,
+      disk_size = "20G"
     }
+
 
   }
 
